751a468aac1f8eb3f3b5a7fd5af4ca8511d00ce7
1SeaMy <deniz.albayrak@gmail.com>
2024-08-19 15:00:51 +0300

    assert_02_negatif

--- src/test/java/DemoqaUpload.java
+++ src/test/java/DemoqaUpload.java
@@ -34,7 +34,7 @@ public class DemoqaUpload {
 
         WebElement uploadMes=driver.findElement(By.id("uploadedFilePath"));
         Assert.assertTrue(uploadMes.isDisplayed());
-        Assert.assertEquals(uploadMes.getText(),"C:\\fakepath\\1.jpg");
+        Assert.assertEquals(uploadMes.getText(),"C:\\fakepath\\1.jpgx");
         System.out.println(uploadMes.getText());
     }
 }
